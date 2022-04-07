7ae8f45ab661250ad5a3eaa09ad313ee24a4e25c
Aftab <31744712+rjaftab786@users.noreply.github.com>
2022-04-07 20:28:20 +0530

    loops

--- PracticeLoops.java
+++ PracticeLoops.java
@@ -7,11 +7,15 @@ public class PracticeLoops {
 
         int n = sc.nextInt();
 
-        int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            sum = sum + i;
+        // int sum = 0;
+        // for (int i = 0; i <= n; i++) {
+        // sum = sum + i;
 
-            System.out.print4(sum);
+        // }
+        // System.out.print(sum);
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i * n);
         }
     }
 
