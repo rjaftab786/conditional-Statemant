83740360e9e584c4b7dee5535a750024d4eb8126
Aftab <31744712+rjaftab786@users.noreply.github.com>
2022-04-07 19:29:57 +0530

    loops

---
+++ Loops.java
@@ -0,0 +1,25 @@
+public class Loops {
+    public static void main(String[] args) {
+        // for(int counter = 0; counter < 10; counter = counter + 1) {
+        // System.out.println("Hello World");
+        // }
+
+        // counter ++ = > counter = counter + 1
+
+        // for (int counter = 0; counter < 11; counter++) {
+        // System.out.print(counter + " ");
+        // }
+
+        // for (int i = 0; i < 11; i++) {
+        // System.out.print(i);
+        // }
+
+        int i = 0;
+
+        while (i < 11) {
+            System.out.println(i++);
+        }
+
+    }
+
+}
