4709e53470b092bc69177c66eba3445af70a7189
Aftab <31744712+rjaftab786@users.noreply.github.com>
2022-04-11 19:50:53 +0530

    pattern

--- Patterns/FloydTriangle.java
+++ Patterns/FloydTriangle.java
@@ -1,5 +1,16 @@
 public class FloydTriangle {
     public static void main(String[] args) {
-        
+        int n = 5;
+        int number = 1;
+
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print( number+ " " );
+                number++; // number =number +1
+
+            }
+            System.out.println( );
+        }
     }
 }
